987fcbb004a672f1ec8d9b3da40a5f1481cb9a70
Jess Nelson <jemane.19@gmail.com>
2023-09-20 20:26:27 -0600

    v2

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 let currentYear = document.querySelector('#year')
-document.querySelector('#year').textContent = new Date().toDateString();
+document.querySelector('#year').textContent = 2023;
 let oLastModif = document.querySelector('#lastModified')
 document.querySelector('#lastModified').textContent = new Date(document.lastModified).toDateString();
