0cebad5c7a00350f65f4a3dac6e6c413956cbd84
Jess Nelson <jemane.19@gmail.com>
2023-09-27 17:55:22 -0600

    site plan

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,11 @@ let currentYear = document.querySelector('#year')
 document.querySelector('#year').textContent = 2023;
 let oLastModif = document.querySelector('#lastModified')
 document.querySelector('#lastModified').textContent = new Date(document.lastModified).toDateString();
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
