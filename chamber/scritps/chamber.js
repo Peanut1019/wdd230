752156a9f5f3a57fec5dee8f715522a273823b88
Jess Nelson <jemane.19@gmail.com>
2023-10-13 21:09:42 -0600

    bom

--- chamber/scritps/chamber.js
+++ chamber/scritps/chamber.js
@@ -11,3 +11,37 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("body");
+const spotlights = document.querySelector(".spotlights");
+const weather = document.querySelector(".weather");
+const events = document.querySelector(".events");
+const hero = document.querySelector(".hero");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☾")) {
+		main.style.background = "#000";
+		spotlights.style.color = "#0f0326";
+		spotlights.style.background = "#5e9cae";
+		weather.style.color = "#5e9cae";
+		weather.style.background = "#0f0326";
+		events.style.color = "#5e9cae";
+		events.style.background = "#0f0326";
+		hero.style.color = "#5e9cae";
+		hero.style.background = "#0f0326";
+		modeButton.textContent = "☼";
+		modeButton.color = "#0f0326";
+	} else {
+		main.style.background = "aliceblue";
+		main.style.color = "#000";
+		spotlights.style.color = "#5e9cae";
+		spotlights.style.background = "#0f0326";
+		weather.style.color = "#0f0326";
+		weather.style.background = "#5e9cae";
+		events.style.color = "#0f0326";
+		events.style.background = "#5e9cae";
+		hero.style.color = "#0f0326";
+		hero.style.background = "#5e9cae";
+		modeButton.textContent = "☾";
+	}
+});
