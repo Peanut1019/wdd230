332ef76216bc58a7c764edb0a6dbfa71763c6cea
Jess Nelson <jemane.19@gmail.com>
2023-10-20 19:51:02 -0600

    06pt2

---
+++ chamber/scritps/windchill.js
@@ -0,0 +1,9 @@
+const temp = 66;
+const speed = 4;
+if (temp <= 50, speed > 3) {
+    wchill = 35.74 + (0.6215 * temp) - (35.75 * speed^.16) + (.4275 * temp * speed^.16)
+}
+else {
+    wchill = "N/A"
+}
+// <=50°F and>3.0mph
